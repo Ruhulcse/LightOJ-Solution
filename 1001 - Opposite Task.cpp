8549a9b19a8e2f6e34cc1f85725d14e923100574
Ruhul amin <ruhul.cse7862@gmail.com>
2018-05-31 10:06:19 +0600

    Beginner

---
+++ 1001 - Opposite Task.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int num;
+    cin>>num;
+    while(num--)
+    {
+        int n;
+        cin>>n;
+        int r=n/2;
+        cout<<r<<" "<<n-r<<endl;
+    }
+    return 0;
+}
